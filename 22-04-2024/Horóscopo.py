b10b7141f2a85aa122c30841096bbdb0fb8742fe
João Vitor Ribeiro <167105008+JVRPer@users.noreply.github.com>
2024-04-22 10:54:03 -0300

    Create Horóscopo.py

---
+++ 22-04-2024/Horóscopo.py
@@ -0,0 +1,30 @@
+
+seu_mes = input("Digite seu mês de aniversário por completo: ")
+
+if seu_mes == "Janeiro":
+        print("Seu signo é Python!")
+elif seu_mes == "Fevereiro":
+        print('Seu signo é Fortran!')
+elif seu_mes == "Março":
+        print('Seu signo é Rust')
+elif seu_mes == "Abril":
+        print("Seu signo é Go!")
+elif seu_mes == "Maio":
+        print("Seu signo é Java!")
+elif seu_mes == "Junho":
+        print("Seu signo é JavaScript!")
+elif seu_mes == "Julho":
+        print("Seu signo é C")
+elif seu_mes == "Agosto":
+        print("Seu signo é PHP!")
+elif seu_mes == "Setembro":
+        print("Seu signo é Heskell!")
+elif seu_mes == "Outubro":
+        print("Seu signo é Perl!")
+elif seu_mes == "Novembro":
+        print("Seu signo é Swift!")
+elif seu_mes == "Dezembro":
+        print("Seu signo é Kotlin!")
+else:
+    print("Você não é um programador!")
+
